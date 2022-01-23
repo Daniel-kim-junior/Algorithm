08338a58f9257efbcbe192928fe1ec43f30bf4a6
jio-kim <now20412041@gmail.com>
2022-01-24 03:29:16 +0900

    자카드 유사도2

--- P 뉴스클러스터링.js
+++ P 뉴스클러스터링.js
@@ -2,10 +2,10 @@ function solution(str1, str2) {
   // 정규 표현식을 이용해 특수문자와 숫자를 제외한 문자들만 부분집합에 넣는다.
   function reg(str) {
     let arr = [];
-    let re = /[A-za-z]{2}/;
+    let law = str.toUpperCase();
     for (let i = 0; i < str.length - 1; i++) {
-      let st = str.substr(i, 2);
-      if (re.match(st)) {
+      let st = law.substr(i, 2);
+      if (st.match(/[A-Za-z]{2}/)) {
         arr.push(st);
       }
     }
@@ -17,8 +17,8 @@ function solution(str1, str2) {
   let union = 0;
   let dup = 0;
 
-  let map = new Map([...tmp1, ...tmp2]);
-  map.forEach((item) => {
+  let set = new Set([...tmp1, ...tmp2]);
+  set.forEach((item) => {
     let num1 = tmp1.filter((x) => item === x).length;
     let num2 = tmp2.filter((x) => item === x).length;
     union += Math.max(num1, num2);
