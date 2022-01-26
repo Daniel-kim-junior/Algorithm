a7ba7e02885d08a487714c28af536f434a08af3e
jio-kim <now20412041@gmail.com>
2022-01-26 09:15:49 +0900

    뒤에서 생각하는법

---
+++ P 아이언슈트.js
@@ -0,0 +1,22 @@
+function solution(n) {
+  let use = 1;
+  let answer;
+  // 뒤에서 접근하는 생각도 좀 하자
+
+  while (n !== 1) {
+    if (n % 2 !== 0) {
+      n = n - 1;
+      use++;
+    } else {
+      n = n / 2;
+    }
+  }
+  if (use > 0) {
+    answer = use + 1;
+  } else {
+    answer = 1;
+  }
+  return answer;
+}
+
+console.log(solution(6));
